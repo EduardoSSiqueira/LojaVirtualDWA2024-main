7356d6bf1cd724b371627ab37d1a2381c23c0090
Eduardo <eduardosouto2014@gmail.com>
2024-09-11 16:51:45 -0300

    criando endpoint de autenticação para o admin

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends, Path, Response
+from fastapi import APIRouter, Depends, Path, Request, Response
 from fastapi.responses import JSONResponse
 
+from dtos.alterar_pedido_dto import AlterarPedidoDto
 from dtos.alterar_produto_dto import AlterarProdutoDto
+from dtos.entrar_dto import EntrarDto
 from dtos.id_produto_dto import IdProdutoDto
 from dtos.inserir_produto_dto import InserirProdutoDto
 from dtos.problem_details_dto import ProblemDetailsDto
@@ -9,6 +11,8 @@ from models.pedido_model import EstadoPedido
 from models.produto_model import Produto
 from repositories.pedido_repo import PedidoRepo
 from repositories.produto_repo import ProdutoRepo
+from repositories.usuario_repo import UsuarioRepo
+from util.auth_jwt import conferir_senha, criar_token
 
 
 router = APIRouter(prefix="/manager")
@@ -46,20 +50,30 @@ async def alterar_produto(inputDto: AlterarProdutoDto):
     return JSONResponse(pd.to_dict(), status_code=404)
 
 @router.post("/alterar_pedido", status_code=204)
-async def alterar_pedido(inputDto: AlterarProdutoDto):
+async def alterar_pedido(inputDto: AlterarPedidoDto):
     if PedidoRepo.alterar_estado(inputDto.id, inputDto.estado.value): return None
-    pd = ProblemDetailsDto("int", f"O produto com id <b>{inputDto.id}</b> não foi encontrado.", "value_not_found", ["body", "id"])
+    pd = ProblemDetailsDto("int", f"O pedido com id <b>{inputDto.id}</b> não foi encontrado.", "value_not_found", ["body", "id"])
     return JSONResponse(pd.to_dict(), status_code=404)
 
 @router.get("/obter_pedido/{id_pedido}")
-async def obter_pedidos(id_pedido: int = Path(..., title="Id do Pedido", ge=1)):
-    pedido = PedidoRepo.obter_por_periodo(id_pedido)
-    if not pedido: return pedido
-    pd = ProblemDetailsDto("int", f"O produto com id <b>{id_pedido}</b> não foi encontrado.", "value_not_found", ["body", "id"])
+async def obter_pedido(id_pedido: int = Path(..., title="Id do Pedido", ge=1)):
+    pedido = PedidoRepo.obter_por_id(id_pedido)
+    if pedido: return pedido
+    pd = ProblemDetailsDto("int", f"O pedido com id <b>{id_pedido}</b> não foi encontrado.", "value_not_found", ["body", "id"])
     return JSONResponse(pd.to_dict(), status_code=404)
 
-@router.get("/obter_pedidos_por_estado{estado}")
+@router.get("/obter_pedidos_por_estado/{estado}")
 async def obter_pedidos_por_estado(estado: EstadoPedido = Path(..., title="Estado do Pedido")):
-    pedidos = PedidoRepo.obter_pedido_por_estado(estado.value)
+    pedidos = PedidoRepo.obter_todos_por_estado(estado.value)
     return pedidos
-   
+
+@router.post("/entrar")
+async def entrar(entrar_dto: EntrarDto):
+    cliente_entrou = UsuarioRepo.obter_por_email(entrar_dto.email)
+    if ((not cliente_entrou)
+        or (not cliente_entrou.senha)
+        or (not conferir_senha(entrar_dto.senha, cliente_entrou.senha))):
+        pd = ProblemDetailsDto("str", f"Credenciais inválidas. Certifique-se de que está cadastrado e de que sua senha está correta.", "value_not_found", ["body", "email", "senha"])
+        return JSONResponse(pd.to_dict(), status_code=404)
+    token = criar_token(cliente_entrou.id, cliente_entrou.nome, cliente_entrou.email, cliente_entrou.perfil)
+    return JSONResponse({"token": token})
